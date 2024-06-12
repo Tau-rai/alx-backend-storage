6f2a11f305c8b0fe35c5b942c0a9c7028681f95c
Tau <taumasaire@gmail.com>
2024-06-12 19:40:43 +0200

    adding country-users script

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,9 @@
--- This file has a script that creates a table with unique users
+-- This scripts creates a table to store the users of a country
+
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
+    country VARCHAR(255) NOT NULL ENUM('US', 'CO', 'TN') DEFAULT 'US',
     PRIMARY KEY (id)
 );
